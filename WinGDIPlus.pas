da9c936f56d029c58a4f4fd855e4a65c868519ba
František Milt <frantisek.milt@gmail.com>
2023-05-20 17:32:57 +0200

    Initial commit, 2023-05-20 17:32 CEST

---
+++ WinGDIPlus.pas
@@ -0,0 +1,16 @@
+unit WinGDIPlus;
+
+{$IF not(defined(MSWINDOWS) or defined(WINDOWS))}
+  {$MESSAGE FATAL 'Unsupported operating system.'}
+{$IFEND}
+
+{$IFDEF FPC}
+  {$MODE ObjFPC}
+{$ENDIF}
+{$H+}
+
+interface
+
+implementation
+
+end.
